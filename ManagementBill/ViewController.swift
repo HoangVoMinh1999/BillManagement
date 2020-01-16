31bcc623c82eb0f25df37904548a24e76197af0c
Vo Minh Hoang <hoangvm.hiephoa@gmail.com>
2020-01-16 08:52:43 -0800

    Initial Commit

---
+++ ManagementBill/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ManagementBill
+//
+//  Created by Vo Minh Hoang on 1/16/20.
+//  Copyright © 2020 Vo Minh Hoang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
